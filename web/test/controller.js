fa97fba14192f1b617292625cd7b8174663fe8ab
niedongsheng <niedongsheng@doctorwork.com>
2018-01-10 11:20:37 +0800

    控制器

--- web/test/controller.js
+++ web/test/controller.js
@@ -2,6 +2,23 @@ module.exports = {
 
     helloWorld: function(req,res) {
         res.send("dddd");
+    },
+
+    empList: function(req,res) {
+        res.send([
+            {
+                name:'哈哈',
+                age: '234'
+            },
+            {
+                name:'牛郎',
+                age: '234'
+            },
+            {
+                name:'织女',
+                age: '234'
+            },
+        ])
     }
 
 
