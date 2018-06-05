689fdc2342eab4166e4ee55c9b462e1c17224a16
niedongsheng <niedongsheng@doctorwork.com>
2018-06-05 20:10:49 +0800

    跨域bug

--- js/util/cors.js
+++ js/util/cors.js
@@ -3,4 +3,5 @@ module.exports = function cors(res) {
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
 }
